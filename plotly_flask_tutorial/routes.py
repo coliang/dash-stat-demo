a01f81686f57d7d4cce40805f5922e57c9c2e65c
colin <colin.ag25@gmail.com>
2024-10-23 19:01:31 -0400

    redirect in data path

--- plotly_flask_tutorial/routes.py
+++ plotly_flask_tutorial/routes.py
@@ -1,10 +1,11 @@
 """Routes for parent Flask app."""
 from flask import current_app as app
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 import psycopg2
 
 @app.route("/")
 def home():
+    datatext = request.args.get('text', default="Generate some data", type = str)
     """Home page of Flask Application."""
     return render_template(
         "index.jinja2",
@@ -13,6 +14,7 @@ def home():
         template="home-template",
         body="This is a homepage served with Flask.",
         base_url=request.base_url,
+        datatext=datatext,
     )
 
 @app.route("/data")
@@ -23,4 +25,14 @@ def data_func():
     res = cur.fetchone()
     cur.close()
     conn.close()
-    return (str(res))
+
+    return redirect(url_for("home", text=str(res)))
+    # return render_template(
+    #     "index.jinja2",
+    #     title="Plotly Dash Flask Tutorial",
+    #     description="Embed Plotly Dash into your Flask applications.",
+    #     template="home-template",
+    #     body="This is a homepage served with Flask.",
+    #     base_url=request.base_url,
+    #     datatext=str(res),
+    # )
